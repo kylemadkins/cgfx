9a2ab6d5f676b91617b69afa6e6d6aed40ae8418
Kyle Adkins <kyle.m.adkins@gmail.com>
2025-05-20 22:04:34 -0400

    Init window

--- Makefile
+++ Makefile
@@ -3,30 +3,27 @@ UNAME_S := $(shell uname -s)
 CC = gcc
 CFLAGS =
 LDFLAGS =
-ONAME =
+ONAME = cgfx
 
 ifeq ($(UNAME_S), Linux)
 	CFLAGS = -I/usr/include/SDL2
 	LDFLAGS = -lSDL2
-	ONAME = cgfx
 endif
 
 ifeq ($(UNAME_S), Darwin)
 	CFLAGS = -I/usr/local/include/SDL2
 	LDFLAGS = -lSDL2
-	ONAME = cgfx
 endif
 
 ifeq ($(OS), Windows_NT)
 	CFLAGS = -Iexternal/mingw/SDL2-2.32.6/include
 	LDFLAGS = -Lexternal/mingw/SDL2-2.32.6/lib -lmingw32 -lSDL2main -lSDL2
-	ONAME = cgfx.exe
 endif
 
 all: build copy-dll
 
 build:
-	$(CC) $(CFLAGS) src/main.c -o ${ONAME} $(LDFLAGS)
+	$(CC) $(CFLAGS) -Wall -std=c99 src/*.c -o ${ONAME} $(LDFLAGS)
 
 copy-dll:
 ifeq ($(OS), Windows_NT)
--- src/main.c
+++ src/main.c
@@ -2,21 +2,45 @@
 
 #include <SDL2/SDL.h>
 
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int init_window(SDL_Window** window, SDL_Renderer** renderer) {
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        fprintf(stderr, "Couldn't initialize SDL\n%s\n", SDL_GetError());
+        return 1;
+    }
+
+    *window = SDL_CreateWindow(
+        "CGFX",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        SCREEN_WIDTH,
+        SCREEN_HEIGHT,
+        0
+    );
+
+    if (!*window) {
+        fprintf(stderr, "Failed to create window\n%s\n", SDL_GetError());
+        return 1;
+    }
+
+    *renderer = SDL_CreateRenderer(*window, -1, 0);
+
+    if (!*renderer) {
+        fprintf(stderr, "Failed to create renderer\n%s\n", SDL_GetError());
+        return 1;
+    }
+
+    return 0;
+}
+
 int main(int argc, char* argv[]) {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        printf("SDL_Init Error: %s\n", SDL_GetError());
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    if (init_window(&window, &renderer) != 0) {
         return 1;
     }
 
-    SDL_Window* win = SDL_CreateWindow("Hello SDL2", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
-    if (!win) {
-        printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
-        SDL_Quit();
-        return 1;
-    }
-
-    SDL_Delay(2000);
-    SDL_DestroyWindow(win);
-    SDL_Quit();
     return 0;
 }
